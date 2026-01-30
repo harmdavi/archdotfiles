1e964d2f4c4195a8256fe3dd7f723143678f3597
harmdavi <harmondrum@gmail.com>
2026-01-29 22:21:19 -0700

    worked more on my nvim config

---
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,27 @@
+return{
+{
+    "mason-org/mason.nvim",
+    config = function()
+	    require("mason").setup()
+    end
+},
+
+
+{
+{
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+	config = function()
+		require("mason-lspconfig").setup {
+    ensure_installed = { "lua_ls", "rust_analyzer" },
+}
+	end
+    },
+},
+
+
+}
+}
